4304d74aa324b504cda5089db13ab6454242fbf7
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-05-19 16:05:24 +0900

    Update train_target_model.py

--- train_target_model.py
+++ train_target_model.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     batch_size = 256
 
     # Define what device we are using
-    print("CUDA Available: ", torch.cuda.is_available())
+    print("CUDA Available: ", torch.cuda.is_available()) #GPUが利用可能か
     device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
 
     mnist_dataset = torchvision.datasets.MNIST('./dataset', train=True, transform=transforms.ToTensor(), download=True)
@@ -31,13 +31,13 @@ if __name__ == "__main__":
             train_imgs, train_labels = data
             train_imgs, train_labels = train_imgs.to(device), train_labels.to(device)
             logits_model = target_model(train_imgs)
-            loss_model = F.cross_entropy(logits_model, train_labels)
+            loss_model = F.cross_entropy(logits_model, train_labels) #クロスエントロピー誤差
             loss_epoch += loss_model
             opt_model.zero_grad()
             loss_model.backward()
             opt_model.step()
 
-        print('loss in epoch %d: %f' % (epoch, loss_epoch.item()))
+        print('loss in epoch %d: %f' % (epoch, loss_epoch.item())) #損失を出力
 
     # save model
     targeted_model_file_name = './MNIST_target_model.pth'
@@ -48,10 +48,10 @@ if __name__ == "__main__":
     mnist_dataset_test = torchvision.datasets.MNIST('./dataset', train=False, transform=transforms.ToTensor(), download=True)
     test_dataloader = DataLoader(mnist_dataset_test, batch_size=batch_size, shuffle=True, num_workers=1)
     num_correct = 0
-    for i, data in enumerate(test_dataloader, 0):
+    for i, data in enumerate(test_dataloader, 0): #enumerate()のカッコ内にリスト等を指定、iとdataに番号(0,1,2,...)と要素を入れる
         test_img, test_label = data
         test_img, test_label = test_img.to(device), test_label.to(device)
         pred_lab = torch.argmax(target_model(test_img), 1)
         num_correct += torch.sum(pred_lab==test_label,0)
 
-    print('accuracy in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test)))
+    print('accuracy in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test))) #正確さを出力
