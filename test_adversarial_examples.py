3bc53703b7a646181928163268b03b6ab25ad52a
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-05-30 14:30:41 +0900

    Update test_adversarial_examples.py

--- test_adversarial_examples.py
+++ test_adversarial_examples.py
@@ -15,22 +15,23 @@ gen_input_nc = image_nc
 print("CUDA Available: ",torch.cuda.is_available())
 device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
 
-# load the pretrained model
+# load the pretrained model 学習済みモデルをロードする
 pretrained_model = "./MNIST_target_model.pth"
 target_model = MNIST_target_net().to(device)
 target_model.load_state_dict(torch.load(pretrained_model))
 target_model.eval()
 
-# load the generator of adversarial examples
+# load the generator of adversarial examples 敵対的サンプルのジェネレータをロードする
 pretrained_generator_path = './models/netG_epoch_60.pth'
 pretrained_G = models.Generator(gen_input_nc, image_nc).to(device)
 pretrained_G.load_state_dict(torch.load(pretrained_generator_path))
 pretrained_G.eval()
 
-# test adversarial examples in MNIST training dataset
+# test adversarial examples in MNIST training datase　MNISTトレーニングデータセットに含まれる敵対的サンプルをテストする
 mnist_dataset = torchvision.datasets.MNIST('./dataset', train=True, transform=transforms.ToTensor(), download=True)
 train_dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
 num_correct = 0
+#enumerate()のカッコ内にリスト(train_dataloader)等を指定、iとdataに番号(0,1,2,...)と要素を格納、開始値は0に指定
 for i, data in enumerate(train_dataloader, 0):
     test_img, test_label = data
     test_img, test_label = test_img.to(device), test_label.to(device)
@@ -43,10 +44,10 @@ for i, data in enumerate(train_dataloader, 0):
 
 print('MNIST training dataset:')
 print('num_correct: ', num_correct.item())
-print('accuracy of adv imgs in training set: %f\n'%(num_correct.item()/len(mnist_dataset)))
+print('accuracy of adv imgs in training set: %f\n'%(num_correct.item()/len(mnist_dataset)))  # num_correct.item() ÷ 60000
 
 # test adversarial examples in MNIST testing dataset
-mnist_dataset_test = torchvision.datasets.MNIST('./dataset', train=False, transform=transforms.ToTensor(), download=True)
+mnist_dataset_test = torchvision.datasets.MNIST('./dataset', train=False, transform=transforms.ToTensor(), download=True) #train = Falseバージョン
 test_dataloader = DataLoader(mnist_dataset_test, batch_size=batch_size, shuffle=False, num_workers=1)
 num_correct = 0
 for i, data in enumerate(test_dataloader, 0):
@@ -60,5 +61,5 @@ for i, data in enumerate(test_dataloader, 0):
     num_correct += torch.sum(pred_lab==test_label,0)
 
 print('num_correct: ', num_correct.item())
-print('accuracy of adv imgs in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test)))
+print('accuracy of adv imgs in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test)))  # num_correct.item() ÷ 10000
 
