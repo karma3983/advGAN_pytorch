f86167aedbd39918b9262c2fd366306e72e81552
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-06-01 16:12:06 +0900

    Update train_target_model.py

--- train_target_model.py
+++ train_target_model.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     print("CUDA Available: ", torch.cuda.is_available()) #GPUが利用可能か
     #コマンドラインから実行され、GPUが利用可能なら"cuda"、それ以外は"cpu"を「使用できるデバイス名」として変数に格納
     device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+    print("使用デバイス：",device)
 
     #'./dataset'はデータセットが存在するディレクトリ
     #train=Trueならtrain-image-idx3-ubyteからデータセット作成、falseならt10k-images-idx3-ubyteで作成
@@ -25,7 +26,7 @@ if __name__ == "__main__":
                                                train=True, 
                                                transform=transforms.ToTensor(), 
                                                download=True)
-    # mnist_datasetからサンプルを取得し、訓練データ（ミニバッチ）を作成　shuffle=シャッフルするかどうか　num_workers=並列実行数
+    # mnist_datasetからサンプルを取得し、訓練データ（ミニバッチ）を作成　shuffle=実行する度ランダムにするかどうか　num_workers=並列実行数
     train_dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
 
     # training the target model　対象モデルの学習
@@ -33,20 +34,28 @@ if __name__ == "__main__":
     target_model.train() #model.train()：ネットワークを学習モードに(DropoutとBatchNormに影響するフラグ)
     opt_model = torch.optim.Adam(target_model.parameters(), lr=0.001) #optim=最適化、lrは学習率(0.1以下なら収束)
     epochs = 40
-    for epoch in range(epochs):
+    for epoch in range(epochs): #0から40
         loss_epoch = 0
         if epoch == 20:
             opt_model = torch.optim.Adam(target_model.parameters(), lr=0.0001) #lrは学習率(0.1以下なら収束)
         #enumerate()のカッコ内にリスト(train_dataloader)等を指定、iとdataに番号(0,1,2,...)と要素を格納、番号の開始値は0に指定
         for i, data in enumerate(train_dataloader, 0):
             train_imgs, train_labels = data #train_dataloaderの要素を代入
-            train_imgs, train_labels = train_imgs.to(device), train_labels.to(device)
+            train_imgs, train_labels = train_imgs.to(device), train_labels.to(device) # device='cuda:0'が付くだけ
             logits_model = target_model(train_imgs)
             loss_model = F.cross_entropy(logits_model, train_labels) #クロスエントロピー誤差（logits_modelは予測値、train_labelsは正解ラベル）
             loss_epoch += loss_model
             opt_model.zero_grad() #勾配をゼロクリアする(パラメータ更新)
             loss_model.backward() #誤差逆伝播(この際勾配が溜まる)
             opt_model.step() #パラメータをモデルに反映
+            
+            print("番号：",i)
+            print("画像：",train_imgs)
+            print("ラベル：",train_labels)
+            print("ロジットモデル：",logits_model)
+            print("損失モデル：",loss_model)
+            print("損失エポック：", loss_epoch)
+            print("----------------------------------")
 
         print('loss in epoch %d: %f' % (epoch, loss_epoch.item())) #損失を出力
 
@@ -72,6 +81,13 @@ if __name__ == "__main__":
         #argmax：Tensor(PyTorchで必ずデータ構造)の全ての要素の内、最大のインデックスを返す
         pred_lab = torch.argmax(target_model(test_img), 1) #target_model = MNIST_target_net().to(device)　なお、dim(縮小する次元)=1
         num_correct += torch.sum(pred_lab==test_label,0) #pred_lab==test_labelがdimを保持しているか確認
+        
+        print("番号：",i)
+        print("画像：",train_img)
+        print("ラベル：",train_labels)
+        print("画像の最大インデックス（予測値）：",train_labels)
+        print("正解数：",num_correct)
+        print("----------------------------------")
 
     print('accuracy in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test))) #正確さを出力
     
