55729476256bdb3d4cb22202fd2856279ca3e1d8
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-06-01 17:16:34 +0900

    Update train_target_model.py

--- train_target_model.py
+++ train_target_model.py
@@ -85,11 +85,12 @@ if __name__ == "__main__":
         
         print("「テスト」番号：",i)
         #print("画像：",test_img) ずーっと0のTensor
-        print("ラベル：",test_label) #256個*39、最後16個->10000個
+        #print("ラベル：",test_label) #256個*39、最後16個->10000個
         #print("画像の最大インデックス（予測値）：",pred_lab) #256個*39、最後16個->10000個
-        print("一致数：",torch.sum(pred_lab==test_label,0))
-        print("合計一致数：",num_correct)
-        #print("正解数.item：",num_correct.item()) 数字のみ
+        #print("一致数：",torch.sum(pred_lab==test_label,0)) #device='cuda:0'付き
+        #print("合計一致数：",num_correct) #device='cuda:0'付き
+        print("一致数：",torch.sum(pred_lab==test_label,0).item())
+        print("合計一致数：",num_correct.item())
         print("----------------------------------")
 
     print('accuracy in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test))) #正確さを出力
@@ -115,9 +116,10 @@ if __name__ == "__main__":
         #print("画像：",test_img) ずーっと0のTensor
         #print("ラベル：",test_label) #256個*234、最後96個->60000個
         #print("画像の最大インデックス（予測値）：",pred_lab) #256個*234、最後96個->60000個
-        print("一致数：",torch.sum(pred_lab==test_label,0))
-        print("合計一致数：",num_correct)
-        #print("正解数.item：",num_correct.item()) 数字
+        #print("一致数：",torch.sum(pred_lab==test_label,0)) #device='cuda:0'付き
+        #print("合計一致数：",num_correct) #device='cuda:0'付き
+        print("一致数：",torch.sum(pred_lab==test_label,0).item())
+        print("合計一致数：",num_correct.item())
         print("----------------------------------")
 
     print('accuracy in trainning set: %f\n'%(num_correct.item()/len(mnist_dataset_test))) #正確さを出力
