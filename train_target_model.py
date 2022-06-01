424e37ee35ac6394a7adb294cf41096600bb721a
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-06-01 17:04:29 +0900

    Update train_target_model.py

--- train_target_model.py
+++ train_target_model.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         
         print("「テスト」番号：",i)
         #print("画像：",test_img) ずーっと0のTensor
-        #print("ラベル：",test_label) #256個*234、最後96個->60000個
+        print("ラベル：",test_label) #256個*234、最後96個->60000個
         #print("画像の最大インデックス（予測値）：",pred_lab) #256個*234、最後96個->60000個
         print("一致数：",torch.sum(pred_lab==test_label,0))
         print("合計一致数：",num_correct)
