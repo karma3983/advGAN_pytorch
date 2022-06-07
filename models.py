8a43985370dc0c4f0361aca3e2a794cb5dd25cea
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-06-07 15:12:05 +0900

    Update models.py

--- models.py
+++ models.py
@@ -132,9 +132,13 @@ class Generator(nn.Module):
 
     #入力としてTensorを受け取る
     def forward(self, x):
+        print('元のxは、{}',format(x))
         x = self.encoder(x)
+        print('エンコーダー後のxは、{}',format(x))
         x = self.bottle_neck(x)
+        print('ボトルネック後のxは、{}',format(x))
         x = self.decoder(x)
+        print('デコーダー後のxは、{}',format(x))
         return x
 
 
