ffeea15f6f2943dc4495b036b67bbc42cea72704
karma3983 <104195323+karma3983@users.noreply.github.com>
2022-06-10 16:19:45 +0900

    Update train_target_model.py

--- train_target_model.py
+++ train_target_model.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
         num_correct += torch.sum(pred_lab==test_label,0) #予測値＝ラベル、pred_lab==test_labelがdimを保持しているか確認
         
         print("「テスト」番号：",i)
-        print("画像：",test_img) ずーっと0のTensor
+        print("画像：",test_img) #ずーっと0のTensor
         print("ラベル：",test_label) #256個*39、最後16個->10000個
         print("画像の最大インデックス（予測値）：",pred_lab) #256個*39、最後16個->10000個
         print("一致数：",torch.sum(pred_lab==test_label,0)) #device='cuda:0'付き
@@ -116,7 +116,7 @@ if __name__ == "__main__":
         num_correct += torch.sum(pred_lab==test_label,0) #予測値＝ラベル、pred_lab==test_labelがdimを保持しているか確認
         
         print("「学習」番号：",i)
-        print("画像：",test_img) ずーっと0のTensor
+        print("画像：",test_img) #ずーっと0のTensor
         print("ラベル：",test_label) #256個*234、最後96個->60000個
         print("画像の最大インデックス（予測値）：",pred_lab) #256個*234、最後96個->60000個
         print("一致数：",torch.sum(pred_lab==test_label,0)) #device='cuda:0'付き
